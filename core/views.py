f38ef40557431b77ab33f9c243b900ec76c0f114
Robin <robin.schmauch@laposte.net>
2024-12-11 16:52:05 +0100

    Page fonctionnelle de paramètres

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+COLORS = [{"name":"ry","display": "Red and Yellow"},{"name":"rb","display": "Red and Blue"}]
+LANGUAGES = [{"name":"en","display": "English"},{"name":"fr","display": "Français"}]
+
 
 def view_accueil(request: HttpRequest):
     return render(request, './page/acceuil.html') 
 
 def view_parametres(request: HttpRequest):
-    return render(request, './page/parametres.html') 
+    return render(request, './page/parametres.html',{"COLORS": COLORS,"LANGUAGES": LANGUAGES} ) 
