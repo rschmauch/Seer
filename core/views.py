6c0e39e764cb7488bb6e3182e103a3ff417cab11
Robin <robin.schmauch@laposte.net>
2025-01-14 11:09:07 +0100

    Mise en place assistants

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from deep_translator import GoogleTranslator
 from django.http import HttpRequest
 from core.scraping.scraping_functionality import contenu_site
+from core.ai.openaiHandler import newAiThread,askAIfor
 
 COLORS = [{"name":"ry","display": "Red and Yellow"},{"name":"rb","display": "Red and Blue"}]
 LANGUAGES = [{"name":"en","display": "English"},{"name":"fr","display": "Français"},{"name":"de","display": "Deutsch"}]
@@ -25,17 +26,24 @@ def view_scraping(request):
 
         # Scraping du contenu
         contenu = contenu_site(url)
-        
-        
+
+        text = ""
         if contenu:
             for element in contenu:
-                if element.get('text'):  
-                    try:
-                        # Effectuer la traduction avec Google Translator via deep_translator
-                        translated_text = GoogleTranslator(source='auto', target=language).translate(element['text'])
-                        element['text'] = translated_text  # Remplacer le texte original par la traduction
-                    except Exception as e:
-                        print(f"Erreur de traduction pour {element['text']}: {e}")
+                if element.get('text'): 
+                    text = text+element['name']+":"+element['text']+"\n"
+
+        # Traitement par IA:
+        thread = newAiThread()
+        contenu = askAIfor(text,thread)
+        
+        
+
+        try:
+        # Effectuer la traduction avec Google Translator via deep_translator
+            contenu = GoogleTranslator(source='auto', target=language).translate(contenu)
+        except Exception as e:
+            print(f"Erreur de traduction pour {element['text']}: {e}")
         
         return render(request, 'page/acceuil.html', {'contenu': contenu,"COLORS": COLORS,"LANGUAGES": LANGUAGES})
     
